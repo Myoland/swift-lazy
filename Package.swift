17f6df49326391ff57c65d89a154eb18345d6fc5
AFuture <afuture0@outlook.com>
2025-07-16 00:25:16 +0800

    remove nio dependcy

--- Package.swift
+++ Package.swift
@@ -10,34 +10,23 @@ dependencies.append(contentsOf: [
     .package(url: "https://github.com/apple/swift-http-types.git", from: "1.4.0"),
     .package(url: "https://github.com/apple/swift-openapi-runtime", from: "1.8.2"),
     .package(url: "https://github.com/apple/swift-async-algorithms", from: "1.0.0"),
-
+    .package(url: "https://github.com/apple/swift-algorithms", from: "1.2.1"),
 ])
-
-#if os(macOS)
-dependencies.append(contentsOf: [
-    .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.25.2"),
-])
-#endif
 
 let package = Package(
     name: "swift-lazy",
     platforms: [.macOS(.v14), .iOS(.v16)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(name: "LazyKit", targets: ["LazyKit"]),
         .library(name: "TestKit", targets: ["TestKit"]),
         .library(name: "NetworkKit", targets: ["NetworkKit"])
     ],
     dependencies: dependencies,
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "LazyKit",
             dependencies: [
                 .product(name: "HTTPTypes", package: "swift-http-types"),
-                .product(name: "AsyncHTTPClient", package: "async-http-client", condition: .when(platforms: [.macOS])),
-//                .product(name: "AsyncAlgorithms", package: "swift-async-algorithms"),
             ]
         ),
         .target(
