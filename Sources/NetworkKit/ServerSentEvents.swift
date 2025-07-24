c8847c5c0f56222a56b6d160e3476fc65e5fe436
AFuture <afuture0@outlook.com>
2025-07-25 01:49:14 +0800

    update sse

--- Sources/NetworkKit/ServerSentEvents.swift
+++ Sources/NetworkKit/ServerSentEvents.swift
@@ -11,6 +11,19 @@ import LazyKit
 import OSLog
 import SynchronizationKit
 
+enum ASCII {
+    /// The carriage return `<CR>` character.
+    static let cr: UInt8 = 0x0d
+    
+    /// The line feed `<LF>` character.
+    static let lf: UInt8 = 0x0a
+    
+    /// The colon `:` character.
+    static let colon: UInt8 = 0x3a
+    
+    /// The space ` ` character.
+    static let space: UInt8 = 0x20
+}
 
 public struct ServerSentEvent: Sendable {
     public let id: String?
@@ -26,6 +39,7 @@ public extension ServerSentEvent {
 }
 
 
+
 /// A Interpreter for processing server-sent-events
 ///
 /// The Interpreter will process as [specification](https://html.spec.whatwg.org/multipage/server-sent-events.html#event-stream-interpretation) descried.
@@ -34,66 +48,120 @@ public final class ServerSentEventsInterpreter: Sendable {
     private let clientLastEventId: LazyLockedValue<String?> = .init(nil)
     public let logger: Logger = .init(subsystem: "me.afuture.server.sse", category: "interpreter")
 
+    private let fields: LazyLockedValue<[String: String]> = .init([:])
+    
+    private let pendingLine: LazyLockedValue<String> = .init("")
+    
+    private let cachedChunk: LazyLockedValue<[UInt8]> = .init([])
+    
     public func process(buffer: Data) -> [ServerSentEvent] {
-        // Streams must be decoded using the UTF-8 decode algorithm.
-        let raw = String(data: buffer, encoding: ServerSentEvent.encoding)
-        logger.debug("[*] \(raw ?? "nil")")
-
-        guard let raw, !raw.isEmpty else {
-            return []
-        }
-
-        let lines = raw.split(separator: Self.lineRegex, omittingEmptySubsequences: false)
-
-        var events: [ServerSentEvent] = []
-        var fields: [String: String] = [:]
-
-        for line in lines {
-            if line == "" {
-                if let event = buildEvent(fields) {
-                    events.append(event)
-                }
-                fields = [:]
-            }
-
-            if line.starts(with: ":") {
-                continue
-            }
-
-            if line.contains(":") {
-                let keyValuePair = line.split(separator: #/: ?/#, maxSplits: 1)
-                guard let keyView = keyValuePair.first else {
+        let bytes = [UInt8](buffer)
+        
+        var chunks: [[UInt8]] = []
+        do {
+            for byte in bytes {
+                cachedChunk.withLock { $0.append(byte) }
+                let chunk = cachedChunk.withLock { $0 }
+                
+                // when fired, it is guaranteed that the chunk
+                // must contains an empty line.
+                // so that evey chunk correspond to a event.
+                if !chunkCanFire(chunk) {
                     continue
                 }
-                let valueView = keyValuePair.last ?? ""
-
-                let key = String(keyView)
-                let value = String(valueView)
-
-                switch key {
-                case "event":
-                    fields["event"] = value
-                case "id" where !value.isEmpty:
-                    fields["id"] = value
-                case "retry":
-                    // TODO: Support retry logic
-                    break
-                case "data":
-                    fields["data"] = (fields["data"] ?? "").appending(value + "\n")
-                default:
-                    break
-                }
-
-            } else {
-                let key = String(line)
-                fields[key] = ""
+                
+                cachedChunk.withLock { $0 = [] }
+                
+                logger.debug("[*] chunk: \(String(data: .init(chunk), encoding: .utf8)!.debugDescription)")
+                chunks.append(chunk)
             }
         }
+        
+        // Streams must be decoded using the UTF-8 decode algorithm.
+        let utf8StrChunk = chunks.compactMap { String(data: .init($0), encoding: .utf8) }
 
-        logger.debug("[*] \(events)")
-        // NOTICE: If the file ends in the middle of an event, before the final empty line,
-        //         the incomplete event is not dispatched.
-        return events
+        var events: [ServerSentEvent?] = []
+        for chunk in utf8StrChunk {
+            let event = processOneChunk(chunk)
+            events.append(event)
+        }
+        
+        return events.compactMap { $0 }
+    }
+    
+    internal func chunkCanFire(_ chunk: [UInt8]) -> Bool {
+        let len = chunk.count
+        if len < 2 {
+            return false
+        }
+        
+        let last1 = chunk[len - 1]
+        let last2 = chunk[len - 2]
+        
+        if last2 == ASCII.lf && last1 == ASCII.lf {
+            // "\n\n"
+            return true
+        } else if last2 == ASCII.cr && last1 == ASCII.cr {
+            // "\r\r"
+            return true
+        } else if len >= 4, chunk[len - 4] == ASCII.cr && chunk[len - 3] == ASCII.lf && last2 == ASCII.cr && last1 == ASCII.lf {
+            // "\r\n\r\n"
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    public func processOneChunk(_ chunk: String) ->  ServerSentEvent? {
+        let lines = chunk.split(separator: Self.lineRegex, omittingEmptySubsequences: false)
+        for line in lines {
+            if line == "" {
+                // empty line. dispatch the event
+                let event = fields.withLock { fields in
+                    let event = buildEvent(fields)
+                    fields = [:]
+                    return event
+                }
+                
+                return event
+            }
+            
+            if line.starts(with: ":") {
+                // the comment line.
+                continue
+            }
+            
+            if line.starts(with: "event:") {
+                let event = line.split(separator: #/: ?/#, maxSplits: 1).last
+                
+                fields.withLock {
+                    $0["event"] = String(event ?? "")
+                }
+                
+            } else if line.starts(with: "id:") {
+                let value = line.split(separator: #/: ?/#, maxSplits: 1).last
+                if let id = value {
+                    fields.withLock {
+                        $0["id"] = String(id)
+                    }
+                }
+                
+            } else if line.starts(with: "retry:") {
+                // TODO: Support retry logic
+            } else if line.starts(with: "data:") {
+                let data = line.split(separator: #/: ?/#, maxSplits: 1).last
+                fields.withLock {
+                    $0["data"] = ($0["data"] ?? "").appending(String(data ?? "") + "\n")
+                }
+            } else {
+                let key = String(line)
+                fields.withLock {
+                    $0[key] = ""
+                }
+            }
+        }
+        
+        return nil
     }
 
     public func buildEvent(_ fields: [String: String]) -> ServerSentEvent? {
@@ -142,10 +210,9 @@ public final class AsyncServerSentEventsInterpreter: AsyncSequence, Sendable {
 
     public func makeAsyncIterator() -> AnyAsyncSequence<ServerSentEvent>.AsyncIterator {
         let interpreter = ServerSentEventsInterpreter()
-
-        return AnyAsyncSequence(stream.map {
+        return stream.map {
             interpreter.process(buffer: $0)
-        }.flatMap { $0.async }).makeAsyncIterator()
+        }.flatMap { $0.async }.eraseToAnyAsyncSequence().makeAsyncIterator()
     }
 }
 
